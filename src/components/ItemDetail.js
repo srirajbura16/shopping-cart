94a467b2b7c755bf214a5a91cdb1b540244f8c39
Sriraj Bura <sriraj.bura16@gmail.com>
2021-06-12 18:30:04 -0400

    update handleSubmit

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -5,42 +5,48 @@ import { findProduct } from '../items';
 function ItemDetail(props) {
   //cart items -> form submit -> setCartItems(item)
 
-  const [cartItems, setCartItems] = useState();
-  const [amount, setAmount] = useState();
+  const [cartItems, setCartItems] = useState([]);
+  const [amount, setAmount] = useState(1);
 
   function handleChange(e) {
-    const amount = e.target.value;
-    setAmount(amount);
+    const newAmount = parseInt(e.target.value);
+    setAmount(newAmount);
+    // console.log(newAmount, amount);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     const item = findProduct(id);
-    //format Item
-    //setCartItems(item)
+    item.amount = amount;
+    setCartItems(cartItems.concat(item));
+    // console.log(cartItems);
+    // console.log(item, amount, cartItems);
     //return to shop page
   }
   let { id } = useParams();
   const product = findProduct(id);
+  const log = () => {
+    console.log(cartItems);
+  };
 
   return (
     <div>
       {/* {Add image} */}
       <h1>{id}</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>{product.title}</div>
         <div>{product.price}</div>
-        <input type="number" max="5" min="0" placeholder="1" />
+        <input
+          onChange={handleChange}
+          type="number"
+          max="5"
+          min="1"
+          placeholder="1"
+        />
         <input type="submit" value="Add" />
       </form>
+      <button onClick={log}>log</button>
     </div>
   );
 }
-
-//'add to cart' btn -> submit -> add item to card products as obj with the amount. ex:
-// const obj = {
-//   item,
-//   amount,
-// };
-
 export default ItemDetail;
